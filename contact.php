16f68f8fab843e1cffc4fd01725309b42b315b49
rgckd <rgchandar@gmail.com>
2025-11-29 14:28:16 +0530

    Add owner status fields to contact form with conditional plot number and phase inputs

--- contact.php
+++ contact.php
@@ -8,15 +8,32 @@ define( "RECIPIENT_EMAIL", "mycitiownersassociation@gmail.com" );
 $userName = isset( $_POST['name'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['name'] ) : "";
 $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
 $senderPhone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9\+\s]/", "", $_POST['phone'] ) : "";
+$isOwner = isset( $_POST['is_owner'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['is_owner'] ) : "";
+$plotNumbers = isset( $_POST['plot_numbers'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9,]/", "", $_POST['plot_numbers'] ) : "";
+$phases = isset( $_POST['phases'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9,]/", "", $_POST['phases'] ) : "";
 $userSubject = isset( $_POST['subject'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
 
-// If all values exist, send the email
-if ( $userName && $senderEmail && $senderPhone && $userSubject && $message) {
+// If all required values exist, send the email
+if ( $userName && $senderEmail && $senderPhone && $isOwner && $userSubject && $message) {
   $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
   $headers = "From: contact@mycitibidadi.com\r\nReply-To: " . $senderEmail;
   $subject = "New Contact Form: " . $userSubject;
-  $msgBody = "Name: ". $userName . "\nEmail: ". $senderEmail . "\nPhone: ". $senderPhone . "\nSubject: ". $userSubject . "\n\nMessage:\n" . $message;
+  
+  // Build message body with owner information
+  $msgBody = "Name: ". $userName . "\nEmail: ". $senderEmail . "\nPhone: ". $senderPhone . "\n";
+  $msgBody .= "Site Owner at MyCiti Layout: " . $isOwner . "\n";
+  
+  if ($isOwner === "Yes" && ($plotNumbers || $phases)) {
+    if ($plotNumbers) {
+      $msgBody .= "Plot Number(s): " . $plotNumbers . "\n";
+    }
+    if ($phases) {
+      $msgBody .= "Phase(s): " . $phases . "\n";
+    }
+  }
+  
+  $msgBody .= "Subject: ". $userSubject . "\n\nMessage:\n" . $message;
   $success = mail( $recipient, $subject, $msgBody, $headers );
 
   //Set Location After Successsfull Submission
